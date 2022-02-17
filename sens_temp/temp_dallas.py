2e19498b3dc44514342b48e925d23e7e64cb9f41
M4M0M3N <79576670+M4M0M3N@users.noreply.github.com>
2022-02-17 12:55:41 +0100

    Migliorata verifica valore ricevuto
    Alcune volte si riceve un valore nullo. Questo vuol dire che il raspberry non riesce a comunicare con il sensore.
    Per ovviare, ho aggiunto un loop che legge il sensore, per un massimo di 5 volte, finche non riceve un numero.

--- sens_temp/temp_dallas.py
+++ sens_temp/temp_dallas.py
@@ -1,3 +1,12 @@
 from w1thermsensor import W1ThermSensor
-sensor = W1ThermSensor()
-print( sensor.get_temperature() )
+
+for i in range(0,5):                #leggo il sensore per massimo 5 volte, per evitare il blocco dello script
+        sensor = W1ThermSensor()
+        temp = sensor.get_temperature()
+        
+        if temp != "":              #se in una di queste 5 volte ricevo una temperatura valida esco dal cliclo
+                break
+
+print(temp)
+
+
