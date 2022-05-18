562adfb3890915bb37c2a1f16128d7f7af45d9a5
M4M0M3N <79576670+M4M0M3N@users.noreply.github.com>
2022-05-18 16:21:09 +0200

    Aggiunto controllo sensore collegato.

--- sens_temp/temp_dallas.py
+++ sens_temp/temp_dallas.py
@@ -1,12 +1,14 @@
 from w1thermsensor import W1ThermSensor
+from w1thermsensor.errors import NoSensorFoundError
 
 for i in range(0,5):                #leggo il sensore per massimo 5 volte, per evitare il blocco dello script
-        sensor = W1ThermSensor()
-        temp = sensor.get_temperature()
-        
-        if temp != "":              #se in una di queste 5 volte ricevo una temperatura valida esco dal cliclo
-                break
+	try:
+	        sensor = W1ThermSensor()
+        	temp = sensor.get_temperature()
+
+        	if temp != "":              #se in una di queste 5 volte ricevo una temperatura valida esco dal cliclo
+                	break
+	except NoSensorFoundError:
+		temp = "Errore, sensore non trovato"
 
 print(temp)
-
-
